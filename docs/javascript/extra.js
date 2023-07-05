80f1296d91e19ae45dc34379155b8449a3f150f7
Architalia <117321045+ArchItalia@users.noreply.github.com>
2023-07-06 00:27:37 +0100

    Update extra.js

--- docs/javascript/extra.js
+++ docs/javascript/extra.js
@@ -2,3 +2,9 @@
 
  var title = document.getElementsByTagName('title')[0]; 
       title.innerHTML = "Architalia";
+
+ var copy = document.getElementsClassName('md-copyright__highlight')[0];
+      copy..innerHTML = (new Date().getFullYear())
+
+
+
